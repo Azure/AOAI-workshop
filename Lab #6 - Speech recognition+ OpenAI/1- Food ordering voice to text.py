3f7ef62af92ee3b64d7eeb7375c63196f53b9310
denisa-ms <dschlesinger@microsoft.com>
2023-11-16 17:59:04 +0200

    upgrading to openai 1.x

--- Lab #6 - Speech recognition+ OpenAI/1- Food ordering voice to text.py
+++ Lab #6 - Speech recognition+ OpenAI/1- Food ordering voice to text.py
@@ -6,7 +6,7 @@
 import azure.cognitiveservices.speech as speechsdk
 from dotenv import load_dotenv
 import pandas as pd
-import openai
+from openai import AzureOpenAI
 import os
 
 
@@ -18,10 +18,11 @@ OPENAI_DEPLOYMENT_NAME = os.getenv("OPENAI_DEPLOYMENT_NAME")
 OPENAI_MODEL_NAME = os.getenv("OPENAI_MODEL_NAME")
 OPENAI_DEPLOYMENT_VERSION = os.getenv("OPENAI_DEPLOYMENT_VERSION")
 
-openai.api_type = "azure"
-openai.api_base = OPENAI_DEPLOYMENT_ENDPOINT
-openai.api_version = "2023-07-01-preview"
-openai.api_key = OPENAI_API_KEY
+client = AzureOpenAI(
+  azure_endpoint = OPENAI_DEPLOYMENT_ENDPOINT, 
+  api_key=OPENAI_API_KEY,  
+  api_version="2023-05-15"
+)
 
 SPEECH_KEY = os.getenv("SPEECH_KEY")
 SPEECH_REGION = os.getenv("SPEECH_REGION")
@@ -41,23 +42,21 @@ def voice_to_text():
     return speech_recognition_result.text
 
 def call_openAI(text):
-
     message_text = [
-    {"role":"system","content":"You are an assistant designed to extract entities from a food order transcript. Users will enter in a string of text and you will respond with entities you\'ve extracted from the text as a JSON object. Here\'s an example of your output format:[  {    main: {      type: \"vegan burger\",      size: \"large\",      cooking_degree: \"medium\",      toppings: [        {          type: \"lettuce\",          quantity: 1,          size: \"small\"        },        {          type: \"tomato\",          quantity: 1,          size: \"\"        },        {          type: \"onion\",          quantity: 2,          size: \"\"        }      ]    },drinks: {  type: \"pepsi cola\",  size: \"large\",  additionals: [{  type: \"ice\",  quantity: 1,  size: \"small\"},{  type: \"lemon\",  quantity: 1,  size: \"\"}  ]}  }]"},
-    {"role":"user","content":text}]
-
-    completion = openai.ChatCompletion.create(
-    engine=OPENAI_DEPLOYMENT_NAME,
-    messages = message_text,
-    temperature=0.7,
-    max_tokens=800,
-    top_p=0.95,
-    frequency_penalty=0,
-    presence_penalty=0,
-    stop=None
+        {"role":"system","content":"You are an assistant designed to extract entities from a food order transcript. Users will enter in a string of text and you will respond with entities you\'ve extracted from the text as a JSON object. Here\'s an example of your output format:[  {    main: {      type: \"vegan burger\",      size: \"large\",      cooking_degree: \"medium\",      toppings: [        {          type: \"lettuce\",          quantity: 1,          size: \"small\"        },        {          type: \"tomato\",          quantity: 1,          size: \"\"        },        {          type: \"onion\",          quantity: 2,          size: \"\"        }      ]    },drinks: {  type: \"pepsi cola\",  size: \"large\",  additionals: [{  type: \"ice\",  quantity: 1,  size: \"small\"},{  type: \"lemon\",  quantity: 1,  size: \"\"}  ]}  }]"},
+        {"role":"user","content":text}]
+    response = client.chat.completions.create(
+        model=OPENAI_DEPLOYMENT_NAME,
+        messages = message_text,
+        temperature=0.7,
+        max_tokens=800,
+        top_p=0.95,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stop=None
     )
-    return completion.choices[0].message.content
 
+    return response.choices[0].message.content
 
 #I would like to order two hamburgers with French fries, tomato, lettuce and mayonnaise and two large cokes and two ice creams
 if __name__ == "__main__":
